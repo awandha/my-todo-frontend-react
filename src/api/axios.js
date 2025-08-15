96f7183285d9b97ff86198088302febce81e17b3
awandha <awandha.ap@gmail.com>
2025-08-15 16:57:14 +0700

    Adjust URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // Change if your Go backend is hosted elsewhere
+  baseURL: import.meta.env.VITE_API_URL, // Change if your Go backend is hosted elsewhere 
 });
 
 export default api;
